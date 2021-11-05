68c9b4ec62a95eb21525179e65a8a3f66126c8f8
Jennifer Lei <jenniferlei1928@gmail.com>
2021-11-05 02:43:49 +0000

    Updated __init__ function

--- harvest.py
+++ harvest.py
@@ -11,9 +11,14 @@ class MelonType:
     ):
         """Initialize a melon."""
 
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
         self.pairings = []
 
-        # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
